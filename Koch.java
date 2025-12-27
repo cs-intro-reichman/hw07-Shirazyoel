80e257fe53acebd6df76a58fbeeb18635ba133f6
Shiraz Yoel <shiraz.jaul@post.runi.ac.il>
2025-12-27 11:43:41 +0200

    d

--- Koch.java
+++ Koch.java
@@ -8,26 +8,49 @@ public class Koch {
 		//// Uncomment only one block in each test, and remember to compile
 		//// the class whenever you change the test.
 
-        /*
+        
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
 		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
 		curve(Integer.parseInt(args[0]),
 			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-		*/
+		
 
-		/*
+	
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
-		snowFlake(Integer.parseInt(args[0]));
-		*/
+		// snowFlake(Integer.parseInt(args[0]));
+		
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
+		if (n == 0) {
+			StdDraw.line(x1, y1, x2, y2);
+        return;
+		}
+
+		 double x4=  x1 + (x2 - x1) / 3.0;
+    	 double y4 = y1 + (y2 - y1) / 3.0;
+
+		 double x5 = x1 + 2 * (x2 - x1) / 3.0;
+   	     double y5 = y1 + 2 * (y2 - y1) / 3.0;
+
+		double x6 = 0.5 * (x1 + x2) + (Math.sqrt(3) / 6.0) * (y1 - y2);
+   	 	double y6 = 0.5 * (y1 + y2) + (Math.sqrt(3) / 6.0) * (x2 - x1);
+
+	
+		 curve(n - 1, x1, y1, x4, y4); 
+         curve(n - 1, x4, y4, x6, y6); 
+         curve(n - 1, x6, y6, x5, y5); 
+         curve(n - 1, x5, y5, x2, y2); 
+
+		
+
+
+
 		//// Write the rest of your code below.
 	}
 
